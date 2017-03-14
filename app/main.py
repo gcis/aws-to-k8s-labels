1a41f54eda6868c39f41ee10bc633faaf930cf3d
gcis <gabriel.cismondi@onfido.com>
2017-03-14 15:56:05 +0000

    added tags taken from the asg even if not propagated to instance

--- app/main.py
+++ app/main.py
@@ -3,9 +3,7 @@ import boto3
 import json
 import time
 import requests
-
 requests.packages.urllib3.disable_warnings(requests.packages.urllib3.exceptions.InsecureRequestWarning)
-
 
 TOKEN = open('/var/run/secrets/kubernetes.io/serviceaccount/token', 'r').read()
 API_HOST = os.environ['KUBERNETES_SERVICE_HOST']
@@ -13,48 +11,87 @@ REGION = os.environ['REGION'] # todo: get from kubernetes API
 
 s = requests.Session()
 s.headers.update({'Authorization': 'Bearer ' + TOKEN})
+
 ec2 = boto3.client('ec2', region_name=REGION)
+asg = boto3.client('autoscaling')
 
 
 def get_url(url):
+    
     return 'https://' + API_HOST + url
 
 
 def get_nodes():
+    
     nodes = s.get(get_url('/api/v1/nodes'), verify=False)
     return list(map(lambda x: {'name': x['metadata']['name'], 'id': x['spec']['externalID']},
                     json.loads(nodes.content)['items']))
 
 
 def tag_nodes():
+    
     for node in get_nodes():
         print node['id']
-        tags = ec2.describe_instances(
+        
+        tags_ec2 = ec2.describe_instances(
             InstanceIds=[
                 node['id']
             ])['Reservations'][0]['Instances'][0]['Tags']
+        
         labels = {}
-        for tag in tags:
-            key = str(tag['Key']).replace(':', '.').replace('/', '-')
-            value = str(tag['Value']).replace(':', '.').replace('/', '-')
+        
+        autoscaling_group = ''
+        
+        for tag in tags_ec2:
+            
+            str_clean = lambda x: x.replace(':', '.').replace('/', '.')
+            
+            key = str_clean(tag['Key'])
+            value = str_clean(tag['Value'])
+            
             labels.update({key: value})
+            
+            if tag['Key'] == 'aws:autoscaling:groupName':
+                autoscaling_group = tag['Value']
+        
+        if len(autoscaling_group):
+            
+            tags_asg = ec2.describe_auto_scaling_groups(
+                AutoScalingGroupNames=[
+                    autoscaling_group
+                ])['AutoScalingGroups'][0]['Tags']
+            
+            for tag in tags_asg:
+                
+                str_clean = lambda x: x.replace(':', '.').replace('/', '.')
+                
+                key = str_clean(tag['Key'])
+                value = str_clean(tag['Value'])
+                
+                labels.update({key: value})
+        
+
         body = {
             'kind': 'Node',
             'metadata': {
                 'labels': labels
             }
         }
+        
         print labels
-        print get_url('/api/v1/nodes/' + node['name'])
+        
         add_labels = s.patch(get_url('/api/v1/nodes/' + node['name']),
                              json=body,
                              headers={'Content-Type': 'application/merge-patch+json'},
                              verify=False)
-        print add_labels.status_code
-        print '_____________________'
+        
+        print 'Kubernetes API response: ' + add_labels.status_code
+        print '___________________________________________________'
 
 while True:
+    
     time.sleep(60)
+    
     try:
         tag_nodes()
     except Exception as e:
